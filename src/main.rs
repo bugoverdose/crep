f15cf2acb1bf3bcf613a90c311e09c344ac85c47
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-24 00:06:50 +0900

    feat: find file and read content

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
-}
 
-// cargo run -- arg1 arg2
-// Running `target/debug/crep arg1 arg2`
-// [src/main.rs:5] args = [
-//     "target/debug/crep",
-//     "arg1",
-//     "arg2",
-// ]
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for '{}'", query);
+    println!("In file '{}'", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Failed to read file");
+
+    println!("With given text\n{contents}");
+}
