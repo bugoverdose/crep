e90780c2ebb1123e6752f9abb55b9a397c174f1c
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-23 22:18:46 +0900

    feat: pass and print command line argument values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }
+
+// cargo run -- arg1 arg2
+// Running `target/debug/crep arg1 arg2`
+// [src/main.rs:5] args = [
+//     "target/debug/crep",
+//     "arg1",
+//     "arg2",
+// ]
