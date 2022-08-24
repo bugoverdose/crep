fd51a053d4a69b31069bf0f2323e75d70d62bea2
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-24 15:47:35 +0900

    refactor: handle exception with Err instead of expect
    - exit with error code of 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,10 +11,17 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Failed to read file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With given text\n{contents}");
+    Ok(())
 }
 
 struct Config {
